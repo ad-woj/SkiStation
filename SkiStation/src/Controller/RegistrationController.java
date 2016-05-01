993669a6ecc0717b08c96a0c4d6219c4fcd8e2af
ad-woj <mywirtualspace@gmail.com>
2016-05-01 21:57:30 +0200

    Przygotowanie projektu
    Konfiguracja bazy
    Zmapowanie klas
    Przygotowanie struktury projektu
    Dodanie przykładu komunikacji z bazą w Model/TestClass
    Przygotowanie JFrame pod GUI

---
+++ SkiStation/src/Controller/RegistrationController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Adam
+ */
+public class RegistrationController {
+    
+    
+    public void validateForm(JPanel loginPanel)
+    {
+        // Validating inputs ...
+        // Triggering inserting new rows in DB ...
+    }
+}
