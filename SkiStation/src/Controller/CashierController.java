6d58511cb1b7228177b0b33edc518936727a17a8
Marzena Kruszy <marzena.kruszy@artifexmundi.com>
2016-06-05 15:05:17 +0200

    Fix commit

---
+++ SkiStation/src/Controller/CashierController.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import DBClasses.Attraction;
+import DBClasses.Cardusage;
+import DBClasses.Terminal;
+import DBClasses.Users;
+import Tools.HibernateUtil;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTextPane;
+import org.hibernate.Session;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author Bluu
+ */
+public class CashierController {
+ 
+    private static Users selectedUser;
+    private Session s;
+    
+    
+    public CashierController()
+    {
+        s = HibernateUtil.getSessionFactory().openSession();
+    }
+    
+    public List GetUsersList(String text)
+    {
+         List queryResult = s.createCriteria(Users.class).add(Restrictions.disjunction()
+                 .add(Restrictions.like("login", text,MatchMode.ANYWHERE))
+                 .add(Restrictions.like("name", text,MatchMode.ANYWHERE))
+                 .add(Restrictions.like("surname", text,MatchMode.ANYWHERE))).list();
+         
+          return queryResult;     
+    }
+    
+    public Users GetRandomUser()
+    {
+        List queryResult = s.createCriteria(Users.class).list();
+        
+        Random random = new Random();        
+        int randomIndex = Math.abs(random.nextInt()) % queryResult.size();
+         
+        return (Users)queryResult.get(randomIndex);     
+    }
+    
+    public static void SetSelectedUser(Users newUser, JTextPane textPane)
+    {
+        selectedUser = newUser;
+        SetText(textPane);
+    }
+    
+    public void SetSelectedUserWithString(String newUserString, JTextPane textPane)
+    {
+        String convertedString = newUserString.subSequence(newUserString.indexOf("(") + 1, newUserString.indexOf(")")).toString();              
+        List queryResult = s.createCriteria(Users.class).add(Restrictions.like("login", convertedString,MatchMode.EXACT)).list();
+        
+        if(queryResult.size() > 0)
+        {
+            selectedUser = (Users)queryResult.get(0);
+        }
+
+        SetText(textPane);
+    }
+
+    private static void SetText(JTextPane textPane)
+    {
+        textPane.setText("Użytkownik: " + GetUserConvertedName(selectedUser));
+    }
+    
+    public static String GetUserConvertedName(Users user)
+    {
+        return user.getName() + " " + user.getSurname() + " (" + user.getLogin() + ")";
+    }
+    
+    public List GetSkiAttractions()
+    {
+        List queryResult = s.createCriteria(Attraction.class).add(Restrictions.like("type", "stok")).list();
+        
+        return queryResult;
+        
+    }
+    
+    private Date GetDateNow()
+    {
+        DateFormat originalFormat = new SimpleDateFormat("yyyy-MM-dd 00:00:00");
+        Date date = null;
+        
+        try {
+            date = originalFormat.parse("01-21-2013 00:00:00");
+        } catch (ParseException ex) {
+            Logger.getLogger(CashierController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return date;
+    }
+    
+    public int GetSkiAttractionTraffic(Attraction attraction)
+    {
+        int traffic = 0;
+        
+        for (Object terminalObject : attraction.getTerminals()) {
+            Terminal terminal = (Terminal)terminalObject;
+            
+            
+            
+            List queryResult = s.createCriteria(Cardusage.class).add(Restrictions.eq("terminal", terminal))
+                   .list();
+            
+            for (Object result : queryResult) {
+                Cardusage usage = (Cardusage)result;
+                
+                System.out.println("Date: " + usage.getUsedate() + "is after: " + GetDateNow() + " - " + usage.getUsedate().after(GetDateNow()));
+                
+                if(usage.getUsedate().after(GetDateNow()))
+                {
+                    traffic++;
+                }
+            }
+
+            //traffic += queryResult.size();
+        }
+
+         return traffic;
+    }
+}
---
+++ SkiStation/src/Controller/ProductController.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import DBClasses.Pricelist;
+import DBClasses.Product;
+import Tools.HibernateUtil;
+import java.util.Date;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author Rafał
+ */
+public class ProductController {
+    private Session s;
+    
+    public ProductController()
+    {
+        s = HibernateUtil.getSessionFactory().openSession();
+    }
+    
+    public boolean CreateProduct(String newProductName, StringBuilder logger)
+    {
+        if (findProduct(newProductName)!=null) {
+            logger.append("Product with the same name already exist");
+            return false;                 
+        }else{
+            Transaction tr = s.beginTransaction();
+            Product product = new Product();
+            product.setName(newProductName);
+            s.saveOrUpdate(product);
+            tr.commit();
+            logger.append("Added new product with name : " + newProductName);
+            return true;
+        }
+    }
+    
+    public List getProductList()
+    {
+         List quaryResult = s.createCriteria(Product.class).list();
+         return quaryResult;                
+    }
+    
+    
+    
+    public List getProductList(String text)
+    {
+         List quaryResult = s.createCriteria(Product.class).add(Restrictions.like("name", ("%"+text+"%"))).list();
+         return quaryResult;                
+    }
+        
+    public List getPriceListsList()
+    {
+         List quaryResult = s.createCriteria(Pricelist.class).list();
+         return quaryResult;                
+    }  
+    
+    public List getPriceListsList(String text)
+    {
+        Integer id;
+        try{
+        id = Integer.parseInt(text);
+        }catch(Exception e){
+            id =null;
+        }
+             
+        if (id == null) {
+            return getPriceListsList();
+        }else{
+            List quaryResult = s.createCriteria(Pricelist.class).add(Restrictions.eqOrIsNull("pricelistid", id)).list();
+            return quaryResult;  
+        }     
+    }
+                    
+    private Product findProduct(String name)
+    {
+         List quaryResult = s.createCriteria(Product.class).add(Restrictions.like("name", name)).list();
+         Product product;
+          if (quaryResult.size()>0) {
+            product = (Product)quaryResult.get(0);
+          }else{
+            product = null;        
+          }
+          return product;     
+    }
+    
+        public boolean CreatePriceList(String from,String to, StringBuilder logger)
+    {
+        
+        Date fromDate = getDateFromString(from);
+        Date toDate = getDateFromString(to);
+        if (fromDate == null || toDate == null) {
+            logger.append("Date format error");
+            return false;
+        }else{
+            Transaction tr = s.beginTransaction();
+            Pricelist priceList = new Pricelist();
+            priceList.setStartdate(fromDate);
+            priceList.setEnddate(toDate);
+            s.saveOrUpdate(priceList);
+            tr.commit();
+            logger.append("Added new Price List with ID :" + priceList.getPricelistid());
+            return true;
+        }
+    }
+        
+        private Date getDateFromString(String text)
+        {
+            System.out.print(text);
+            String[] dateStrings = text.split("-");
+            try
+            {
+                for (String dateString : dateStrings) {
+                    System.out.println(dateString);
+                }
+            int day=Integer.parseInt(dateStrings[0]);
+            int month = Integer.parseInt(dateStrings[1]);
+            int year = Integer.parseInt(dateStrings[2]);
+            return new Date(year-1900,month-1,day);
+            }catch(Exception e)
+            {
+                return null;
+            }
+            
+        }
+    
+    
+}
