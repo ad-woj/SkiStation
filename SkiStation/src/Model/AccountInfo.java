496fea34ff0dcaac31418811330872dd4d11805b
BluuTM <nav22@interia.pl>
2016-07-14 18:08:04 +0200

    AccountInfo.java documentation

--- SkiStation/src/Model/AccountInfo.java
+++ SkiStation/src/Model/AccountInfo.java
@@ -1,24 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
 /**
- *
- * @author Marzena
+ * Klasa przechowująca dane dotyczące konta użytkownika
+ * @author Marzena, documentation Sebastian
  */
 public class AccountInfo {
+    
+    /** 
+     * Imię właściciela konta
+     */
     public String name;
+    
+     /** 
+     * Nazwisko właściciela konta
+     */
     public String surname;
+    
+     /** 
+     * Login właściciela konta
+     */
     public String login;
+    
+     /** 
+     * ID właściciela konta
+     */
     public String userID;
+    
+     /** 
+     * Numer dokumentu właściciela konta
+     */
     public String documentNumber;
+    
+     /** 
+     * Nazwa ulicy właściciela konta
+     */
     public String street;
+    
+     /** 
+     * Miasto zamieszkania właściciela konta
+     */
     public String city;
+    
+     /** 
+     * Kraj właściciela konta
+     */
     public String country;
+    
+     /** 
+     * Hasło właściciela konta
+     */
     public String password;
+    
+     /** 
+     * Stan konta - nieaktywne jest z punktu widzenia użytkownika nieistniejące
+     */
     public Boolean active;
     
     public AccountInfo() {
